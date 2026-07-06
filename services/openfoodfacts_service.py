38cb7824528d6a1cc6238b1c6f4642779bf4eb71
sammy <samwelmwangi106@gmail.com>
2026-07-06 17:18:49 +0300

    üpdated project

---
+++ services/openfoodfacts_service.py
@@ -0,0 +1,6 @@
+# containing function to communicate with OpenFoodFacts API
+import requests
+
+def get_product_by_barcode(barcode):
+    #fetch product information frm the Api using  a barcode
+    
