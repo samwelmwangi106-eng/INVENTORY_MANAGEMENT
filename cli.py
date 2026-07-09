a38aa328c27cbdae3316937ee6ffd85f0fd5f702
Samwel <samwelmwangi106@gmail.com>
2026-07-09 21:58:30 +0300

    updated the project

---
+++ cli.py
@@ -0,0 +1,6 @@
+# Starts the Inventory Management CLI.
+
+from cli.cli import menu
+
+if __name__ == "__main__":
+    menu()
--- services/database_service.py
+++ services/database_service.py
@@ -1,13 +1,35 @@
-# handles reading and writing to db.json
-import json
-DATABASE_FILE = "db.json"
+# Handles reading and writing data to db.json.
 
+import json
+import os
+
+# Get the project root directory.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Build the full path to db.json.
+DATABASE_FILE = os.path.join(BASE_DIR, "db.json")
+
+
+# Load data from db.json.
 def load_database():
-    #Load all data from db.json
-    with open(DATABASE_FILE,"r") as file:
+
+    # Create the file if it does not exist.
+    if not os.path.exists(DATABASE_FILE):
+
+        database = {
+            "products": []
+        }
+
+        with open(DATABASE_FILE, "w") as file:
+            json.dump(database, file, indent=4)
+
+    # Read the database.
+    with open(DATABASE_FILE, "r") as file:
         return json.load(file)
-    
-def save_database(data):
-    #save updated data back to db.json.
+
+
+# Save data into db.json.
+def save_database(database):
+
     with open(DATABASE_FILE, "w") as file:
-        json.dump(data, file)
+        json.dump(database, file, indent=4)
